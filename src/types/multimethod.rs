8d31969ac860cdc088e5211ca896f98eeeb8029e
Moritz Riede <moritz@mriede.com>
2022-05-14 00:45:45 +0200

    doc: update doc comment

--- src/types/multimethod.rs
+++ src/types/multimethod.rs
@@ -5,7 +5,7 @@ use magc::{
 
 use std::collections::HashMap;
 
-/// A method definition that has one name and many pairs of function signatures and bodies.
+/// A method with a single name and many pairs of function signatures and bodies.
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Multimethod {
     /// The individual signatures and bodies this multimethod is composed of.
