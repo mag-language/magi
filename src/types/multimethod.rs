98e0d1ae3f91b8d9737e07e5316fe533939193d9
Moritz Riede <moritz@mriede.com>
2022-05-19 23:44:22 +0200

    fix: use BTreeMap so we can store this in a HashMap

--- src/types/multimethod.rs
+++ src/types/multimethod.rs
@@ -3,11 +3,11 @@ use magc::{
     Pattern,
 };
 
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 /// A method with a single name and many pairs of function signatures and bodies.
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Multimethod {
     /// The individual signatures and bodies this multimethod is composed of.
-    pub receivers: HashMap<Pattern, Expression>,
+    pub receivers: BTreeMap<Pattern, Expression>,
 }
