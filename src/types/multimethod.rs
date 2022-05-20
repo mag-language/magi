a1c09d585fc5ee1e363b2d9ff831c27645475969
Moritz Riede <moritz@mriede.com>
2022-05-21 00:47:30 +0200

    feat: start implementing call method for the multimethod struct

--- src/types/multimethod.rs
+++ src/types/multimethod.rs
@@ -1,6 +1,12 @@
 use magc::{
     Expression,
+    Method,
     Pattern,
+};
+
+use magc::parser::{
+    ParserResult,
+    ParserError,
 };
 
 use std::collections::BTreeMap;
@@ -10,4 +16,32 @@ use std::collections::BTreeMap;
 pub struct Multimethod {
     /// The individual signatures and bodies this multimethod is composed of.
     pub receivers: BTreeMap<Pattern, Expression>,
+}
+
+impl Multimethod {
+    /// Create a new multimethod from the given [`Method`].
+    pub fn from(method: Method) -> Self {
+        Self {
+            receivers: BTreeMap::new(),
+        }
+    }
+
+    // / Try to match the given call signature with one of the method implementations.
+    pub fn call(&self, signature: Pattern) -> Result<(), ParserError> {
+        for (pattern, body) in &self.receivers {
+            match signature.linearize(pattern.clone()) {
+                Ok(variables) => {
+                    println!("[✓] This is a match");
+                    println!("Extracted variables: {:?}", variables);
+                },
+
+                Err(e) => {
+                    println!("[⨯] This is no match");
+                    return Err(ParserError::NoMatch)
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
