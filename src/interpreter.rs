932344f56caa79ceb8a5d1805f96afc02584b89c
Moritz Riede <moritz@mriede.com>
2022-05-21 00:45:58 +0200

    feat: add interpreter impl calling the new environment constructor

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,3 +3,11 @@ use crate::types::{Environment};
 pub struct Interpreter {
     environment: Environment,
 }
+
+impl Interpreter {
+    pub fn new() -> Self {
+        Self {
+            environment: Environment::new(),
+        }
+    }
+}
