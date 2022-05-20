b6278569e886b5bce5f8434aa53e070726a205b0
Moritz Riede <moritz@mriede.com>
2022-05-21 00:46:23 +0200

    feat: use value and add constructor

--- src/types/environment.rs
+++ src/types/environment.rs
@@ -1,7 +1,16 @@
 use magc::*;
+use super::Value;
 
 use std::collections::HashMap;
 
 pub struct Environment {
-    entries: HashMap<VariablePattern, Expression>,
+    entries: HashMap<VariablePattern, Value>,
+}
+
+impl Environment {
+    pub fn new() -> Self {
+        Self {
+            entries: HashMap::new(),
+        }
+    }
 }
