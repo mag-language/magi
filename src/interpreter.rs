28f2fa43ca26ecf5edff8e6b7d3e2cd59ad7cbc6
Moritz Riede <moritz@mriede.com>
2022-06-02 00:34:41 +0200

    feat: take care of selecting right method receiver using pattern linearization

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -24,16 +24,27 @@ impl Interpreter {
         }
     }
 
-    pub fn evaluate(&mut self, expression: Box<Expression>) -> Result<Box<Expression>, InterpreterError> {
+    /// Interpret a given piece of code.
+    pub fn evaluate(
+        &mut self,
+        // The expression to evaluate.
+        expression: Box<Expression>,
+        // An optional environment used for variables in local scope.
+        environment_opt: Option<Environment>,
+    ) -> Result<Box<Expression>, InterpreterError> {
         match expression.kind {
             ExpressionKind::Method(method) => self.define_method(method),
-            ExpressionKind::Call(call)     => self.call_method(call),
+            ExpressionKind::Call(call)     => self.call_method(call, environment_opt),
+            //ExpressionKind::Infix(infix)   => self.evaluate_infix(infix),
 
             _ => Err(InterpreterError::Unimplemented),
         }
     }
 
-    fn define_method(&mut self, method: Method) -> Result<Box<Expression>, InterpreterError> {
+    fn define_method(
+        &mut self,
+        method: Method,
+    ) -> Result<Box<Expression>, InterpreterError> {
         if let Some(multimethod) = self.methods.get_mut(&method.name) {
             multimethod.define(method.signature.unwrap(), method.body);
         } else {
@@ -50,26 +61,40 @@ impl Interpreter {
         ))
     }
 
-    fn call_method(&mut self, call: Call) -> Result<Box<Expression>, InterpreterError> {
-        let mut does_match = false;
+    fn call_method(
+        &mut self,
+        call: Call,
+        // An optional environment used for variables in local scope.
+        environment_opt: Option<Environment>,
+    ) -> Result<Box<Expression>, InterpreterError> {
+        // This array contains the matching patterns (if any) which will then be sorted by precedence.
+        let mut matching_receivers =  vec![];
 
         if let Some(multimethod) = self.methods.get(&call.name) {
+            // Iterate over each of the possibly matching methods.
             for (sig, body) in &multimethod.receivers {
                 if let Some(ref signature) = call.signature {
-                    match sig.linearize(signature.clone()) {
-                        Ok(extracted_variables) => {
-                            does_match = true;
-                            println!("MATCH! linearizing: {:?} with {:?}", sig, signature);
-                        },
-
-                        Err(e) => {
-                            println!("no match linearizing: {:?} with {:?}", sig, signature);
-                        },
+                    // Compare the method signature to the reference.
+                    if let Ok(extracted_variables) = sig.linearize(signature.clone()) {
+                        matching_receivers.push((signature.clone(), body.clone(), extracted_variables));
                     }
                 }
             }
 
-            println!("does_match = {}", does_match);
+            // Sort the matching receivers so that the most specific pattern is first in line.
+            matching_receivers.sort_by(|a, b| b.0.get_precedence().cmp(&a.0.get_precedence()));
+
+            if matching_receivers.len() > 0 {
+                let (sig, body, vars) = matching_receivers[0].clone();
+                println!("matching_receiver: {:#?}\n\n{:#?}", body, vars);
+
+                // check if single expression, in this case evaluate and return
+                //
+                // otherwise it's a block, iterate and evaluate each of the expressions
+                // using the extracted variables as the function scope.
+            } else {
+                return Err(InterpreterError::NoMatchingReceiver)
+            }
 
             Ok(Box::new(
                 Expression {
@@ -94,6 +119,7 @@ pub enum InterpreterError {
     NoMatchingMultimethod,
 }
 
+#[derive(Debug)]
 pub struct Multimethod {
     pub receivers: HashMap<Pattern, Box<Expression>>,
 }
