dda029b9b90e943f559a53b4594f46cd009c8723
Moritz Riede <moritz@mriede.com>
2022-06-05 00:29:35 +0200

    feat: create new obj module

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,3 +1,9 @@
 pub mod environment;
+pub mod obj;
 
 pub use self::environment::*;
+
+/// A piece of code that can evaluate a specific kind of expression.
+pub trait Visitor {
+    fn parse(&self, interpreter: &mut Interpreter, expr: Box<Expression>) -> InterpreterResult;
+}
