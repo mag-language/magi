d6ba6992f339b687ec6d97b14228eebbe85006d3
Moritz Riede <moritz@mriede.com>
2022-06-04 02:49:53 +0200

    chore: make inner hash map public and add 'from' function

--- src/types/environment.rs
+++ src/types/environment.rs
@@ -4,11 +4,17 @@ use std::collections::HashMap;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Environment {
-    entries: HashMap<VariablePattern, Box<Expression>>,
+    pub entries: HashMap<VariablePattern, Box<Expression>>,
 }
 
 impl Environment {
     pub fn new() -> Self {
+        Self {
+            entries: HashMap::new(),
+        }
+    }
+
+    pub fn from(map: HashMap<VariablePattern, Box<Expression>>) -> Self {
         Self {
             entries: HashMap::new(),
         }
