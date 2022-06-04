b48c74d54a7a03bca052350b9c8193048affebf3
Moritz Riede <moritz@mriede.com>
2022-06-04 02:39:20 +0200

    feat: work on getting method definitions and calls working

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,8 +1,11 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, BTreeMap};
 
 use crate::types::environment::Environment;
 
+use magc::type_system::Typed;
+
 use magc::types::{
+    Literal,
     Expression,
     ExpressionKind,
     Method,
@@ -12,11 +15,13 @@ use magc::types::{
     Pattern,
     PairPattern,
     ValuePattern,
+    VariablePattern,
 };
 
 pub struct Interpreter {
     environment: Environment,
     methods: HashMap<String, Multimethod>,
+    recursion_level: usize,
 }
 
 impl Interpreter {
@@ -24,6 +29,7 @@ impl Interpreter {
         Self {
             environment: Environment::new(),
             methods:     HashMap::new(),
+            recursion_level: 0,
         }
     }
 
@@ -35,6 +41,8 @@ impl Interpreter {
         // An optional environment used for variables in local scope.
         environment_opt: Option<Environment>,
     ) -> Result<Box<Expression>, InterpreterError> {
+        println!("evaluating: --- {:#?}", expression.kind);
+
         match expression.kind {
             ExpressionKind::Method(method) => self.define_method(method),
             ExpressionKind::Call(call)     => self.call_method(call, environment_opt),
@@ -42,6 +50,13 @@ impl Interpreter {
             ExpressionKind::List(_)        => { return Ok(expression) },
             ExpressionKind::Type           => { return Ok(expression) },
             ExpressionKind::Infix(infix)   => self.evaluate_infix(infix, environment_opt),
+            ExpressionKind::Pattern(pattern) => Ok(Box::new(Expression {
+                // Unwrap is safe here because there is always a pattern in the expression.
+                kind: ExpressionKind::Pattern(self.evaluate_pattern(Some(pattern), environment_opt)?.unwrap()),
+                start_pos: expression.start_pos,
+                end_pos: expression.end_pos,
+                lexeme: expression.lexeme,
+            })),
 
             _ => Err(InterpreterError::Unimplemented),
         }
@@ -52,9 +67,9 @@ impl Interpreter {
         method: Method,
     ) -> Result<Box<Expression>, InterpreterError> {
         if let Some(multimethod) = self.methods.get_mut(&method.name) {
-            multimethod.define(method.signature.unwrap(), method.body);
+            multimethod.define(method.signature, method.body);
         } else {
-            let m = Multimethod::from(method.signature.unwrap(), method.body);
+            let m = Multimethod::from(method.signature, method.body);
             self.methods.insert(method.name.clone(), m);
         }
         Ok(Box::new(
@@ -67,64 +82,162 @@ impl Interpreter {
         ))
     }
 
+    fn do_arithmetic(&self, call: Call) -> Result<Box<Expression>, InterpreterError> {
+        if let Some(signature) = call.signature {
+            let pair = self.expect_pair(Box::new(signature))?;
+
+            let num = match call.name.as_str() {
+                "+" => self.expect_int(pair.left)? + self.expect_int(pair.right)?,
+                "-" => self.expect_int(pair.left)? - self.expect_int(pair.right)?,
+                "*" => self.expect_int(pair.left)? * self.expect_int(pair.right)?,
+                "/" => self.expect_int(pair.left)? / self.expect_int(pair.right)?,
+    
+                _ => unreachable!(),
+            };
+
+            Ok(Box::new(Expression {
+                kind: ExpressionKind::Literal(Literal::Int),
+                lexeme: format!(
+                    "{}", 
+                    num,
+                ),
+                start_pos: 0,
+                end_pos: 0,
+            }))
+        } else {
+            Err(InterpreterError::Unimplemented)
+        }
+    }
+
+    fn expect_pair(&self, pattern: Box<Pattern>) -> Result<PairPattern, InterpreterError> {
+        match *pattern {
+            Pattern::Pair(pair) => Ok(pair),
+
+            _ => Err(
+                InterpreterError::UnexpectedType { 
+                    expected: "PairPattern".to_string(),
+                    found: pattern.get_type(),
+                }
+            )
+        }
+    }
+
+    fn expect_int(&self, pattern: Box<Pattern>) -> Result<i64, InterpreterError> {
+        match *pattern {
+            Pattern::Value(ValuePattern { expression }) => {
+                match expression.kind {
+                    ExpressionKind::Literal(Literal::Int) => {
+                        Ok(expression.lexeme.parse::<i64>().unwrap())
+                    },
+
+                    _ => Err(
+                        InterpreterError::UnexpectedType { 
+                            expected: "PairPattern".to_string(),
+                            found: None,
+                        }
+                    ),
+                }
+            },
+
+            _ => Err(
+                InterpreterError::UnexpectedType { 
+                    expected: "Int".to_string(),
+                    found: None,
+                }
+            )
+        }
+    }
+
+    fn evaluate_pattern(
+        &mut self,
+        pattern: Option<Pattern>,
+        environment_opt: Option<Environment>,
+    ) -> Result<Option<Pattern>, InterpreterError> {
+        match pattern {
+            None => Ok(None),
+
+            Some(Pattern::Value(value_pattern)) => Ok(
+                Some(Pattern::Value(ValuePattern {
+                    expression: self.evaluate(value_pattern.expression.clone(), environment_opt)?,
+                }))
+            ),
+
+            Some(Pattern::Variable(variable_pattern)) => {
+                if let Some(env) = environment_opt {
+                    if let Some(expr) = env.entries.get(&variable_pattern) {
+                        Ok(Some(Pattern::Value(ValuePattern {
+                            expression: expr.clone(),
+                        })))
+                    } else {
+                        Err(InterpreterError::NoMatchingVariable)
+                    }
+                } else {
+                    if let Some(expr) = self.environment.entries.get(&variable_pattern) {
+                        Ok(Some(Pattern::Value(ValuePattern {
+                            expression: expr.clone(),
+                        })))
+                    } else {
+                        Err(InterpreterError::NoMatchingVariable)
+                    }
+                }
+            },
+
+            _ => Ok(pattern),
+        }
+    }
+
+    fn get_multimethod(&self, name: &String) -> Result<Multimethod, InterpreterError> {
+        if let Some(multimethod) = self.methods.get(name) {
+            Ok(multimethod.clone())
+        } else {
+            Err(InterpreterError::NoMatchingMultimethod)
+        }
+    }
+
+    /**fn sort_receivers(&self,
+        receivers: HashMap<Option<Pattern>, Box<Expression>>,
+        environment_opt: Option<Environment>,
+    ) -> Result<Vec<(Option<Pattern>, Box<Expression>)>, InterpreterError> {
+
+        let receivers_vec: Vec<(Option<Pattern>, Box<Expression>)> = receivers
+            .iter()
+            .map(|&(reference_sig, body)| (reference_sig, body))
+            .collect();
+
+
+        receivers_vec
+            .iter()
+            .sort_by(|a, b| {
+                b.0.get_precedence().cmp(&a.0.get_precedence())
+            })
+            .collect()
+    }*/
+
     fn call_method(
         &mut self,
         call: Call,
         // An optional environment used for variables in local scope.
         environment_opt: Option<Environment>,
     ) -> Result<Box<Expression>, InterpreterError> {
-        // This array contains the matching patterns (if any) which will then be sorted by precedence.
-        let mut matching_receivers =  vec![];
-
-        if let Some(multimethod) = self.methods.get(&call.name) {
-            // Iterate over each of the possibly matching methods.
-            for (sig, body) in &multimethod.receivers {
-                if let Some(ref signature) = call.signature {
-                    // Compare the method signature to the reference.
-                    if let Ok(extracted_variables) = sig.linearize(signature.clone()) {
-                        matching_receivers.push((signature.clone(), body.clone(), extracted_variables));
-                    }
-                }
-            }
-
-            // Sort the matching receivers so that the most specific pattern is first in line.
-            matching_receivers.sort_by(|a, b| b.0.get_precedence().cmp(&a.0.get_precedence()));
-
-            if matching_receivers.len() > 0 {
-                let (sig, body, vars) = matching_receivers[0].clone();
-                println!("matching_receiver: {:#?}\n\n{:#?}", body, vars);
-
-                // Evaluate a single expression or the children of a block.
-                match body.kind {
-                    ExpressionKind::Block(mut block) => {
-                        let last_child = block.children.pop();
-
-                        for expr in block.children {
-                            self.evaluate(Box::new(expr.clone()), environment_opt.clone());
-                        }
-
-                        return Err(InterpreterError::Unimplemented)
-                    },
-
-                    _ => {
-                        return self.evaluate(body, environment_opt.clone())
-                    },
-                }
-            } else {
-                return Err(InterpreterError::NoMatchingReceiver)
-            }
-
-            Ok(Box::new(
-                Expression {
-                    kind: ExpressionKind::Identifier,
-                    start_pos: 0,
-                    end_pos: 0,
-                    lexeme: call.name,
-                }
-            ))
-        } else {
-            Err(InterpreterError::NoMatchingMultimethod)
+        if self.recursion_level >= 4 {
+            return Err(InterpreterError::TooMuchRecursion)
         }
+
+        println!("HANDLE CALL name: {}", call.name);
+        match call.name.as_str() {
+            "+" | "-" | "*" | "/" => return self.do_arithmetic(call),
+
+            _ => {},
+        }
+
+        Ok(Box::new(
+            Expression {
+                kind: ExpressionKind::Identifier,
+                start_pos: 0,
+                end_pos: 0,
+                lexeme: call.name,
+            }
+        ))
     }
 
     fn pattern_or_value_pattern(&self, expression: Box<Expression>) -> Pattern {
@@ -166,22 +279,40 @@ impl Interpreter {
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum InterpreterError {
     Unimplemented,
+    UnexpectedType { expected: String, found: Option<String> },
     MethodAlreadyExists,
     SignatureAlreadyExists,
     NoMatchingReceiver,
     NoMatchingMultimethod,
+    NoMatchingVariable,
+    TooMuchRecursion,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Multimethod {
-    pub receivers: HashMap<Pattern, Box<Expression>>,
+    pub receivers: Vec<Receiver>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Receiver {
+    pub signature: Option<Pattern>,
+    pub body:      Box<Expression>,
+}
+
+impl Receiver {
+    pub fn from(signature: Option<Pattern>, body: Box<Expression>) -> Self {
+        Self {
+            signature,
+            body,
+        }
+    }
 }
 
 impl Multimethod {
-    pub fn from(signature: Pattern, body: Box<Expression>) -> Self {
-        let mut receivers = HashMap::new();
+    pub fn from(signature: Option<Pattern>, body: Box<Expression>) -> Self {
+        let mut receivers = vec![];
 
-        receivers.insert(signature, body);
+        receivers.push(Receiver::from(signature, body));
 
         Self {
             receivers,
@@ -189,9 +320,9 @@ impl Multimethod {
     }
 
     /// Add a new receiver to this multimethod if it does not already exist.
-    pub fn define(&mut self, signature: Pattern, body: Box<Expression>) -> Result<(), InterpreterError> {
-        if !self.receivers.contains_key(&signature) {
-            self.receivers.insert(signature, body);
+    pub fn define(&mut self, signature: Option<Pattern>, body: Box<Expression>) -> Result<(), InterpreterError> {
+        if let None = self.receivers.iter().find(|recv| recv.signature == signature) {
+            self.receivers.push(Receiver::from(signature, body));
 
             Ok(())
         } else {
@@ -199,11 +330,82 @@ impl Multimethod {
         }
     }
 
-    pub fn call(&self, given_signature: Pattern) -> Result<(), InterpreterError> {
-        for (sig, body) in &self.receivers {
-            println!("{:?}", sig);
-        }
+    pub fn call(&self, call: Call, environment_opt: Option<Environment>) -> Result<Box<Expression>, InterpreterError> {
+        let matching_receivers = self.find_matching_receivers(call, environment_opt)?;
+        /*
+.sort_by(|a, b| {
+    b.0.get_precedence().cmp(&a.0.get_precedence())
+})
+        */
 
-        Ok(())
+        Ok(Box::new(Expression {
+            kind: ExpressionKind::Literal(Literal::Int),
+            lexeme: format!(
+                "{}", 
+                27,
+            ),
+            start_pos: 0,
+            end_pos: 0,
+        }))
     }
+
+    fn find_matching_receivers(&self, 
+        call: Call,
+        environment_opt: Option<Environment>,
+    ) -> Result<Vec<(Environment, Box<Expression>, usize)>, InterpreterError> {
+
+        let matching_receivers = self.receivers
+            .iter()
+            // Filter out receivers if their signature doesn't match.
+            .filter(|receiver| {
+                !self::match_pattern(
+                    receiver.signature.clone(),
+                    call.signature.clone(),
+                )
+            })
+            // Convert the matching receivers to a tuple containing the extracted variables,
+            // the body expression and the pattern's precedence (or specificity).
+            .map(|receiver| {
+                (
+                    // Extract the variables which will be bound to function scope.
+                    self::match_pattern_and_extract(
+                        receiver.signature.clone(),
+                        call.signature.clone()
+                    ),
+                    receiver.body.clone(),
+                    self::get_precedence(call.signature.clone()),
+                )
+            })
+            .collect();
+
+        Ok(matching_receivers)
+    }
+}
+
+fn get_precedence(item: Option<Pattern>) -> usize {
+    match item {
+        Some(pattern) => pattern.get_precedence(),
+        None          => 0,
+    }
+}
+
+fn match_pattern(reference: Option<Pattern>, given: Option<Pattern>) -> bool {
+    match (reference, given) {
+        (None, None) => true,
+        (Some(r), Some(g)) => r.matches_with(g),
+
+        _ => false,
+    }
+}
+
+fn match_pattern_and_extract(reference: Option<Pattern>, given: Option<Pattern>) -> Environment {
+    let map = match (reference, given) {
+        (None, None) => HashMap::new(),
+
+        (Some(r), Some(g)) => r.linearize(g).unwrap(),
+
+        _ => HashMap::new(),
+    };
+
+    Environment::from(map)
 }
