c86f2d7bd0ba32eec69ba8f4d1b174a8294211ac
Moritz Riede <moritz@mriede.com>
2022-06-04 16:10:20 +0200

    chore: update trait implementation

--- src/visitors/mod.rs
+++ src/visitors/mod.rs
@@ -12,8 +12,9 @@ use magc::types::Expression;
 /// A piece of code that knows how to evaluate a specific kind of expression.
 pub trait Visitor {
     fn evaluate(
+        &self,
         interpreter: &mut Interpreter,
         environment_opt: Option<Environment>,
         expr: Expression,
-    ) -> InterpreterResult where Self: Sized;
+    ) -> InterpreterResult;
 }
