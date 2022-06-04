d3b0837e3e4b5bb5402c75f17cd0d6a99ed41775
Moritz Riede <moritz@mriede.com>
2022-06-05 01:31:31 +0200

    chore: move method receiver definition into submodule

---
+++ src/types/multimethod/receiver.rs
@@ -0,0 +1,19 @@
+use magc::types::{
+    Expression,
+    Pattern,
+};
+
+#[derive(Debug, Clone)]
+pub struct Receiver {
+    pub signature: Option<Pattern>,
+    pub body:      Box<Expression>,
+}
+
+impl Receiver {
+    pub fn from(signature: Option<Pattern>, body: Box<Expression>) -> Self {
+        Self {
+            signature,
+            body,
+        }
+    }
+}
