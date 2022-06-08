51dfc990ad6db1f32a1345d1fe9e0f8c0665569c
Moritz Riede <moritz@mriede.com>
2022-06-08 21:19:44 +0200

    feat: implement new value visitor

---
+++ src/interpreter/visitors/value.rs
@@ -0,0 +1,33 @@
+use crate::types::{Environment, Obj};
+use crate::types::ObjKind::*;
+use super::Visitor;
+
+use crate::interpreter::{
+    Interpreter,
+    InterpreterResult,
+    InterpreterError,
+};
+
+use magc::type_system::Typed;
+
+pub struct ValueVisitor;
+
+impl Visitor for ValueVisitor {
+    fn evaluate(
+        &self,
+        interpreter: &mut Interpreter,
+        optional_env: Option<Environment>,
+        obj: Obj,
+    ) -> InterpreterResult {
+
+        /// Values simply evaluate to themselves.
+        match &obj.kind {
+            Int(_) | UInt(_) | Float(_) | String(_) => Ok(Box::new(obj)),
+
+            _ => Err(InterpreterError::UnexpectedType { 
+                found: obj.get_type(),
+                expected: "Int | UInt | String | Float".to_string(),
+            })
+        }
+    }
+}
