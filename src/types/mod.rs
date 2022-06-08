32995b5c5edf277bb81edf844c3e7128ce3a0b23
Moritz Riede <moritz@mriede.com>
2022-06-08 19:49:19 +0200

    chore: remove old imports and visitor definition

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,19 +1,9 @@
 pub mod environment;
 pub mod obj;
 pub mod multimethod;
+pub mod pattern;
 
 pub use self::environment::Environment;
 pub use self::multimethod::*;
 pub use self::obj::{Obj, ObjKind};
-
-use crate::interpreter::{
-    Interpreter,
-    InterpreterResult,
-};
-
-use magc::types::Expression;
-
-/// A piece of code that can evaluate a specific kind of expression.
-pub trait Visitor {
-    fn parse(&self, interpreter: &mut Interpreter, expr: Box<Expression>) -> InterpreterResult;
-}
+pub use self::pattern::*;
